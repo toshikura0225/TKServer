8456db454d595f714245ea5f9d0d0699bbb7b994
toshikura <t_kuramata@yahoo.co.jp>
2019-10-12 21:16:15 +0900

    PUGからEJSに変更

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+//app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
