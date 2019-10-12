83ae70d7001565a5e44e5de580e3f5f9002cd93b
toshikura <t_kuramata@yahoo.co.jp>
2019-10-13 07:14:47 +0900

    温度測定を実装中

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express',
+        temps: DStemps
+    });
 });
+
+var DStemps = [];
+router.get('/set_temps', function (req, res) {
+
+    if (req.query.temps) {
+        DStemps = JSON.parse(req.query.temps);
+        console.log(DStemps);
+    }
+
+    res.send("OK");
+});
+
 
 module.exports = router;
---
+++ temps.js
@@ -0,0 +1,47 @@
+﻿'use strict';
+
+// 動作させるプラットフォーム
+const platform = require('os').platform();
+console.log(platform);
+
+const fs = require('fs');
+
+
+var tempsID = setInterval(() => {
+
+    get_wire("28-030897790b03", (temp) => {
+        console.log(temp);
+    });
+
+}, 5000);
+
+function get_wire(ds_id, next) {
+
+    if (platform === 'linux') {
+        let path = `sys/bus/w1/devices${ds_id}/w1_slave`;
+
+        fs.readFile(path, 'utf-8', (err, data) => {
+
+            // 例外処理
+            if (err) { throw err; }
+            let temp = null;
+
+            if (data.match(/Yes/)) {
+
+                let matches = data.match(/t=(\d+)/);
+                temp = parseInt(matches[1]) / 1000;
+
+            } else {
+                temp = null;
+            }
+
+            next(temp);
+        });
+    } else if (platform === 'win32') {
+        let max = 30, min = -10;
+        let temp = Math.random() * (max + 1 - min) + min;
+        next(temp);
+    }
+
+}
+
