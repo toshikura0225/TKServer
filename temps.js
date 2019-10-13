4e2f682aa835298f797042068b0faa9bc087f2e8
toshikura <t_kuramata@yahoo.co.jp>
2019-10-13 21:19:14 +0900

    configファイル作成

--- temps.js
+++ temps.js
@@ -1,21 +1,26 @@
 ﻿'use strict';
 
+const fs = require('fs');
+const async = require('async');
+const request = require('request');
+
+//var deviceList = {
+//    "28-030897790b03": { "offset": -0.984, "value": 0 },    // 外気温
+//    "28-0308977986cf": { "offset": -0.57, "value": 0 },     // 室温（上部）
+//    "28-030897791fc8": { "offset": 0.73, "value": 0 },      // 給水槽
+//    "28-030997796347": { "offset": 0.62, "value": 0 },      // 生育槽
+//    "28-031097791505": { "offset": 0, "value": 0 }          // 室温（下部）
+//};
+
+
 // 動作させるプラットフォーム
 const platform = require('os').platform();
 console.log(platform);
 
-const fs = require('fs');
 
-const request = require('request');
-
-const async = require('async');
-var deviceList = {
-    "28-030897790b03": { "offset": -0.984, "value": 0 },    // 外気温
-    "28-0308977986cf": { "offset": -0.57, "value": 0 },     // 室温（上部）
-    "28-030897791fc8": { "offset": 0.73, "value": 0 },      // 給水槽
-    "28-030997796347": { "offset": 0.62, "value": 0 },      // 生育槽
-    "28-031097791505": { "offset": 0, "value": 0 }          // 室温（下部）
-};
+const temps_config = require('./temps.json');
+console.log(`deviceList = ${temps_config}`);
+const deviceList = temps_config["deviceList"];
 
 function main() {
 
@@ -32,7 +37,7 @@ function main() {
         if (err) throw err;
 
         let temps = Object.keys(deviceList).map(id => deviceList[id]["value"]);
-        
+        console.log(`Measured ${temps}`);
 
         request({
             method: 'get',
@@ -45,7 +50,7 @@ function main() {
 
 }
 
-//var tempTimerID = setInterval(main , 5000);
+var tempTimerID = setInterval(main , 60 * 1000);
 main();
 
 function get_wire(ds_id, next) {
