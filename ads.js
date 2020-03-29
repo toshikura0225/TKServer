79107d0bd04e27b0f31681583214ac5a6e72dddf
toshikura <t_kuramata@yahoo.co.jp>
2020-03-29 12:25:26 +0900

    AD読み込みをmcp-spi-adcに変更

---
+++ ads.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+const mcpadc = require('mcp-spi-adc');
+
+const tempSensor = mcpadc.open(6, {speedHz: 20000}, err => {
+  if (err) throw err;
+ 
+  setInterval(_ => {
+    tempSensor.read((err, reading) => {
+      if (err) throw err;
+ 
+      console.log(reading.value * 5);
+      //console.log((reading.value * 5.0) * 100);
+    });
+  }, 1000);
+});
