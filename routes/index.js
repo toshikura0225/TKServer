dd10c2fa09635d4fd0373a30ed00f706dc40f762
toshikura <t_kuramata@yahoo.co.jp>
2021-02-14 09:12:24 +0900

    queryをhtmlとして表示

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,19 @@
 var express = require('express');
 var router = express.Router();
 
+var req_url = null;
+
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+
+    let vvv = "OK";
+    if (req.url.indexOf("?") != -1) {
+        req_url = req.url
+    }
+    else {
+        vvv = req_url;
+    }
+    res.render('index', { title: vvv });
 });
 
 module.exports = router;
