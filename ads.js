15ab56a843c53eba40ca99624bd33f821131ccd5
MyRaspi3B <t_kuramata@yahoo.co.jp>
2022-07-17 11:05:39 +0900

    2022/07/17に久しぶりに開いた

--- ads.js
+++ ads.js
@@ -1,6 +1,10 @@
 ﻿'use strict';
 
 const mcpadc = require('mcp-spi-adc');
+const gpio = require('rpi-gpio');
+
+const SW_PIN = 40;
+gpio.setup(SW_PIN, gpio.DIR_OUT);
 
 const tempSensor = mcpadc.open(6, {speedHz: 20000}, err => {
   if (err) throw err;
@@ -12,5 +16,14 @@ const tempSensor = mcpadc.open(6, {speedHz: 20000}, err => {
       console.log(reading.value * 5);
       //console.log((reading.value * 5.0) * 100);
     });
+    
+    gpio.write(SW_PIN, true);
+    console.log("switch on");
+
+    setTimeout(() => {
+        gpio.write(SW_PIN, false);
+        console.log("switch off");
+    }, 500);
+    
   }, 1000);
 });
